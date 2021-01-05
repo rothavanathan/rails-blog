15b6a2f0b8d4d58f0074605fa052a9609f2b0251
Romesh Thavanathan <romeshthavanathan@gmail.com>
2021-01-05 21:16:32 +0000

    create Comment model and associations with Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,12 +3,12 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
   
-  def new
-    @article = Article.new
-  end
-
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
   end
 
   def edit
@@ -33,6 +33,13 @@ class ArticlesController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+   
+    redirect_to articles_path
   end
     
   private
